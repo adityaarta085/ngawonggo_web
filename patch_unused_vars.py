bce6915a7db69bab258c03ae4318d928dd23ca38
adityaarta085 <adityaarta085@gmail.com>
2026-04-18 12:15:01 +0700

    delete

--- patch_unused_vars.py
+++
@@ -1,7 +0,0 @@
-with open('src/views/AdminPage/components/PemerintahanManager.js', 'r') as f:
-    content = f.read()
-
-content = content.replace("  Divider,\n", "")
-
-with open('src/views/AdminPage/components/PemerintahanManager.js', 'w') as f:
-    f.write(content)
